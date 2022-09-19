154ae9c6a5c6d3b6359cd76a24e6949be7b3e038
RAVAILLE James <contact@algowin.fr>
2022-09-19 14:02:00 +0200

    Ajoutez des fichiers projet.

---
+++ DateTimeWripper.cs
@@ -0,0 +1,12 @@
+﻿namespace MySharedAssembly
+{
+    public class DateTimeWrapper
+    {
+        public DateTime CurrentDateTime => DateTime.Now;
+
+        public override string ToString()
+        {
+            return this.CurrentDateTime.ToLongTimeString();
+        }
+    }
+}
